276859973b2d2e0621528246496d1a3e008dbb57
Vivek Venugopal <143856591+V-Gits@users.noreply.github.com>
2025-01-19 23:46:38 +0530

    Add files via upload

---
+++ ExtraQuestions/Module-1/SwappingWithoutTemp.py
@@ -0,0 +1,5 @@
+NUM1 = int(input("Enter the Number 1: "))
+NUM2 = int(input("Enter the Number 2: "))
+
+NUM1,NUM2 = NUM2,NUM1
+print(f"\n\tNumber at Number 1 position is: {NUM1}\n\tNumber at Number 2 position is: {NUM2}")
